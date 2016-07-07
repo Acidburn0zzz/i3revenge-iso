17af46d30053a2e6b92aebdc81ccd135ba5fc41f
ajjames31 <ajjames31@gmail.com>
2016-07-06 19:25:09 -0500

    corrections to code

--- i3install.sh
+++ i3install.sh
@@ -6,7 +6,12 @@
 pacstrap /mnt i3 dmenu lxterminal gedit vim lxappearance numix-themes deepin-icon-theme
 
 #Copying config file
+mkdir /mnt/home/ajjames31/.config
 mkdir /mnt/home/ajjames31/.config/i3
-cp /i3config/i3config /mnt/home/ajjames31/.config/i3/config
+cp i3config/i3config /mnt/home/ajjames31/.config/i3/config
+clear
 
 echo "Install is complete!"
+echo "Press any key to reboot, or hit control c to stay in the live environment."
+read
+reboot 
